1f3f77f4068c5f9a9d08a375a48640e4ecb35483
mughalfahim <154056473+mughalfahim@users.noreply.github.com>
2025-09-01 11:50:21 +0500

    Update script.js
    Signed-off-by: mughalfahim <154056473+mughalfahim@users.noreply.github.com>

--- script.js
+++ script.js
@@ -24,28 +24,20 @@ window.onscroll = () => {
                 links.classList.remove('active');
                 document.querySelector('header nav a[href*=' + id +']').classList.add('active');
             });
-            // active sections for animation on scroll 
             sec.classList.add('show-animate');
         }
-        // if want to use animation that repeats on scroll use this 
         else {
             sec.classList.remove('show-animate');
-
         }
     });
-    // sticky header 
-    let header = document.querySelector('header');
 
+    let header = document.querySelector('header');
     header.classList.toggle('sticky',window.scrollY > 100);
 
-    // remove toggle icon and navbar when click navbar links (scroll) 
     menuIcon.classList.remove('bx-x'); 
     navbar.classList.remove('active');
     
-    // animation footer on scroll 
     let footer = document.querySelector('footer');
-
     footer.classList.toggle('show-animate', this.innerHeight + this.scrollY >= 
     document.scrollingElement.scrollHeight);
-
 }
